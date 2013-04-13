f6af693030b3e3184877d18b00863022d4e897e9
Raphael Gruber <raphi011@gmail.com>
2013-04-14 00:30:42 +0200

    Lässt sich jetzt kompilierne, Transpose + IsSymmetricH funktioniert noch nicht

--- 4/AsciiImage.java
+++ 4/AsciiImage.java
@@ -1,139 +1,147 @@
 import java.util.Properties;
 
-public class AsciiImage 
+public class AsciiImage
 {
 	private int width = 0;
 	private int height = 0;
-	
-	private StringBuffer image;
-	
- public AsciiImage()
- {
- 	 
- }
- 
- public boolean addLine(String line)
- {
- 	if (width == 0)
- 	{
- 		if (line.length() < 1)
- 			return false;
- 		
- 		width = line.length();
- 		image = new StringBuffer();	
- 	}
- 	else if (line.length() != width)
- 		return false;
- 	
- 	image.append(line);
- 	height++;
- 	
- 	return true;
- }
- 
- public int getWidth()
- {
- 	 return width;
- }
- 
- public int getHeight()
- {
- 	 return height;
- }
- 
- public String toString()
- {
- 	 String imageString = "";
- 	 
- 	 for (int i = 0; i < height; i++)
- 	 {
- 	 	 int substringStart = i * width;
- 	 	 int substringEnd = substringStart + width;
- 	 	 
- 	 	 imageString += image.substring(substringStart, substringEnd) ;
- 	 
- 	 	 if (i != height - 1)
- 	 	 	 imageString += System.getProperty("line.separator");
- 	 }	
 
-	return imageString; 	 
- }
- 
- public int getUniqueChars()
- {
- 	String imageString = toString();
- 	  
-        int count = 0;
-        
-        while (imageString.length() > 0) 
-        {
-            count++;
-            imageString = imageString.replace(image.substring(0,1), "");
-        }
-        
-        return count;
- }
- 
- public void flipV()
- {
- 	 StringBuffer invertedImage = new StringBuffer();
- 	
- 	 for (int i = height - 1; i >= 0; i--)
- 	 {
- 	 	 int substringStart = i * width;
- 	 	 int substringEnd = substringStart + width;
- 	 	 
- 	 	 invertedImage.append(image.substring(substringStart, substringEnd)); 
- 	 }
- 	 
- 	 image = invertedImage;
- }
- 
- public void transpose()
- {
- 	 int newWidth = height;
- 	 int newHeight = width;
- 	 
- 	 StringBuffer transposedImage = new StringBuffer();
- 	 
- 	 
- }
- 
- private char getPixel(int x, int y)
- {
- 	return image.charAt(y * width + x);	 
- }
- 
- private void setPixel(int x, int y, char c)
- {
- 	 
- }
- 
- public boolean isSymmetricH()
- {
- 	 
- }
- 
- public void fill(int x, int y, char c)
- {
- 	  char charToReplace = image[y].charAt(x);
+	private String image;
 
-        // Zeichen ersetzen!
-        image[y] = replaceCharAt(image[y], x, c);
+	public AsciiImage()  {}
 
-        //links
-        if (x - 1 >= 0 && image[y].charAt(x - 1) == charToReplace)
-            fill(image, x - 1, y, c);
-        // oben
-        if (y - 1 >= 0 && image[y - 1].charAt(x) == charToReplace)
-            fill(image, x, y - 1, c);
-        // rechts
-        if (x + 1 < image[y].length() && image[y].charAt(x + 1) == charToReplace)
-            fill(image, x + 1, y, c);
-        // unten
-        if (y + 1 < image.length && image[y + 1].charAt(x) == charToReplace)
-            fill(image, x, y + 1, c);
- }	
-	
-	
+	public boolean addLine(String line)
+	{
+		if (width == 0)
+		{
+			if (line.length() < 1)
+				return false;
+
+			width = line.length();
+			image = "";
+		}
+		else if (line.length() != width)
+			return false;
+
+		image += line;
+		height++;
+
+		return true;
+	}
+
+	public int getWidth()
+	{
+		return width;
+	}
+
+	public int getHeight()
+	{
+		return height;
+	}
+
+	public String toString()
+	{
+		String imageString = "";
+
+		for (int i = 0; i < height; i++)
+		{
+			int substringStart = i * width;
+			int substringEnd = substringStart + width;
+
+			imageString += image.substring(substringStart, substringEnd) ;
+
+			if (i != height - 1)
+				imageString += System.getProperty("line.separator");
+		}
+
+		return imageString;
+	}
+
+	public int getUniqueChars()
+	{
+		String imageString = image;
+
+		int count = 0;
+
+		while (imageString.length() > 0)
+		{
+			count++;
+			imageString = imageString.replace(imageString.substring(0,1), "");
+		}
+
+		return count;
+	}
+
+	public void flipV()
+	{
+		String invertedImage = "";
+
+		for (int i = height - 1; i >= 0; i--)
+		{
+			int substringStart = i * width;
+			int substringEnd = substringStart + width;
+
+			invertedImage += image.substring(substringStart, substringEnd);
+		}
+
+		image = invertedImage;
+	}
+
+	public void transpose()
+	{
+		int newWidth = height;
+		int newHeight = width;
+
+		StringBuffer transposedImage = new StringBuffer();
+
+
+	}
+
+	private char getPixel(int x, int y)
+	{
+		return image.charAt(y * width + x);
+	}
+
+	private void setPixel(int x, int y, char c)
+	{
+		String newImage = "";
+
+		int charPos = y* width + x;
+
+		if (charPos != 0)
+			newImage = image.substring(0, charPos - 1);
+
+		newImage += c;
+
+		if (charPos + 1 != image.length())
+			newImage += image.substring(charPos + 1);
+
+		image = newImage;
+	}
+
+	/* public boolean isSymmetricH()
+	{
+
+	} */
+
+	public void fill(int x, int y, char c)
+	{
+		int pos = y * width + x;
+		char charToReplace = image.charAt(pos);
+
+		// Zeichen ersetzen!
+		setPixel(x, y, c);
+
+		//links
+		if (x - 1 >= 0 && image.charAt(pos - 1) == charToReplace)
+			fill(x - 1, y, c);
+		// oben
+		if (y - 1 >= 0 && image.charAt(pos - width) == charToReplace)
+			fill(x, y - 1, c);
+		// rechts
+		if (x + 1 < width && image.charAt(pos + 1) == charToReplace)
+			fill(x + 1, y, c);
+		// unten
+		if (y + 1 < height && image.charAt(pos + width) == charToReplace)
+			fill(x, y + 1, c);
+	}
 }
--- 4/AsciiShop.java
+++ 4/AsciiShop.java
@@ -1,10 +1,104 @@
 import java.util.Scanner;
 
-public class AsciiShop 
+public class AsciiShop
 {
-    public static void main(String[] args) 
-    {
-    		    
-    }
-    
-}				
+	static String uniqueCharsCommand = "uniqueChars";
+	static String flipCommand = "flip-v";
+	static String transposeCommand = "transpose";
+	static String fillCommand = "fill";
+
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+
+		String command = sc.next();
+
+		try {
+			if (!command.equals("read") || !sc.hasNextInt())
+				throw new InputMismatchException();
+
+			int lineCount = sc.nextInt();
+			sc.nextLine();
+
+			// AsciiImage initialisieren
+			AsciiImage image = new AsciiImage();
+
+			for (int i = 0; i < lineCount; i++) {
+				// Wenn weniger Zeilen als angegeben oder die nŠchste Zeile nicht korrekt ist ... Fehler
+				if (!sc.hasNextLine() || !image.addLine(sc.nextLine()))
+					throw new InputMismatchException();
+			}
+
+
+			if (sc.hasNextLine()) {
+				while (sc.hasNextLine()) {
+					DoOperation(sc, image); }
+			}
+
+			// Ausgabe des Ergebnisses
+			System.out.println(image.toString());
+			System.out.println(image.getWidth() + " " + image.getHeight());
+		}
+		catch (InputMismatchException e) {
+			System.out.println("INPUT MISMATCH");
+		}
+		catch (OperationFailedException e) {
+			System.out.println("OPERATION FAILED");
+		}
+	}
+
+	public static void DoOperation(Scanner scanner, AsciiImage image) throws InputMismatchException, OperationFailedException
+	{
+		String fillParamString = scanner.nextLine().trim();
+
+		if (fillParamString.length() == 0)
+			return;
+
+		String[] tokens = fillParamString.split(" ");
+		String cmd = tokens[0];
+
+		if (cmd.equals(uniqueCharsCommand))
+			System.out.println(image.getUniqueChars());
+		else if (cmd.equals(flipCommand))
+			image.flipV();
+		else if (cmd.equals(transposeCommand))
+			image.transpose();
+		else if (cmd.equals(fillCommand))
+		{
+			int x;
+			int y;
+			char c;
+
+			// Es mŸssen 4 Argumente sein
+			if (tokens.length != 4)
+				throw new InputMismatchException();
+
+			// Das zu erzeichende Zeichen darf nur 1 lang sein
+			if (tokens[3].length() != 1)
+				throw new InputMismatchException();
+
+			// Befehl ŸberprŸfen
+			if (!tokens[0].equals("fill"))
+				throw new InputMismatchException();
+
+			c = tokens[3].charAt(0);
+
+			// Koordinaten parsen
+			try {
+				x = Integer.parseInt(tokens[1]);
+				y = Integer.parseInt(tokens[2]);
+			} catch (NumberFormatException e) {
+				throw new InputMismatchException(); }
+
+			if (y >= image.getHeight() || y < 0 || x >= image.getWidth() || x < 0)
+				throw new OperationFailedException();
+
+			image.fill(x,y,c);
+		}
+		else
+			throw new InputMismatchException();
+	}
+}
+
+class InputMismatchException extends Exception {}
+class OperationFailedException extends Exception {}
