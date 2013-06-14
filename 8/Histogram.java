8ac6a4361cbbe13f4202a15f93d00de6579029e7
Raphael Gruber <raphi011@gmail.com>
2013-06-14 21:52:34 +0200

    Started with Histogram

--- 8/Histogram.java
+++ 8/Histogram.java
@@ -1,11 +1,42 @@
 public class Histogram 
 {
 	public static AsciiImage getHistogram(AsciiImage img)
-	{
-		AsciiImage imgCopy = new AsciiImage(img);
+	{	
+		int height = 16;
+		int width = img.getCharset().length();
+		int charset = createCharset(img);
+
+		AsciiImage histogram = new AsciiImage(width,height, charset);
+		
+		
+		
+		
+		
+		
+		
 		return imgCopy;	
+	}
+	
+	private void drawHistogram(AsciiImage histogram, AsciiImage img)
+	{
+		
+	
+	
+	
+	}
 
+	private String createCharset(AsciiImage img);
+	{
+		String imgCharset = img.getCharset();
+		String charset = "1234567890.#";
 
+		for (int i = 0; i < imgCharset.length(); i++)
+		{
+			if (charset.indexOf(imgCharset.charAt(i)) >= 0)
+				charset += imgCharset.charAt(i);
+		}
+
+		return charset;
 	}
 
 }
